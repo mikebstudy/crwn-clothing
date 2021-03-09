0e9cd151072b480829eff3a37f3e97076f5b2276
MikeB StudyMode <mikebstudy@gmail.com>
2021-03-08 20:58:40 -0800

    L076.Setup: Setup initial main.RouterSample from L076 React Router Dom

---
+++ RouterLearning.md
@@ -0,0 +1,12 @@
+# RouterLearning
+
+### main.RouterSample
+
+### L076.Setup: Setup initial main.RouterSample from L076 React Router Dom
+
+- Created branch from main.Setup
+- Cleaned App.css
+- Added App.FinalSourceFromSite.js from L76 React Router Dom source provided
+- Setup App.js to correspond to beginning of lesson of L76
+-  Fixed title in index.html - <title>Crown Clothing - Router Sample</title>
+- Setup index.js to wrap App.js in <BrowserRouter> tag
--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
 import './App.css';
+
+const HomePage = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsList = props => {
+  return (
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+    </div>
+  );
+};
+
+const TopicDetail = props => {
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE:</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      Crown Clothing - Setup
+    <div>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/topics' component={TopicsList} />
+      <Route path='/topics/:topicId' component={TopicDetail} />
     </div>
   );
 }
